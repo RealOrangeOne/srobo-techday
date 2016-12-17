905fb071ebf26d2a4213c283e22fbd91127ada2a
Jake Howard <git@theorangeone.net>
2016-12-17 11:25:44 +0000

    Add clock

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 $('#date').text(
   moment().format('MMMM Do YYYY')
 );
+
+setInterval(function(){
+  $('#time').each(function () {
+    $(this).text(
+      moment().format('hh:mm:ss')
+    )
+  });
+}, 1000);
